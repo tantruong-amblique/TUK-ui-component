603baf50a64828c693746448b9d14632dc5a188a
Akash52 <ac8572611@gmail.com>
2021-05-31 12:10:28 +0530

    Tabs

--- src/components/Premium/application-ui/navigation/tabs/Tabs.js
+++ src/components/Premium/application-ui/navigation/tabs/Tabs.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import FullWidthBar from '../breadcrumbs/full_width_bar'
 import BarWU from './bar_with_underline'
+import FullWTU from './full_width_tabs_with_underline'
 import TabsIP from './tabs_in_pills'
 import TabsPOG from './tabs_in_pills_on_gray'
 import TabsPBC from './tabs_in_pills_with_brand_color'
@@ -11,13 +11,38 @@ import TabsWUAI from './tabs_with_underline_and_icons'
 const Tabs = () => {
   return (
     <>
+      <div className="text-4xl animate-pulse m-3 font-semibold">Tabs</div>
+      <div className="text-xl animate-bounce  m-3 font-semibold">
+        bar_with_underline
+      </div>
       <BarWU />
-      <FullWidthBar />
+      <div className="text-xl animate-bounce  m-3 font-semibold">
+        full_width_tabs_with_underline
+      </div>
+      <FullWTU />
+      <div className="text-xl animate-bounce  m-3 font-semibold">
+        tabs_in_pills_on_gray
+      </div>
       <TabsPOG />
+      <div className="text-xl animate-bounce  m-3 font-semibold">
+        tabs_in_pills_with_brand_color
+      </div>
       <TabsPBC />
+      <div className="text-xl animate-bounce  m-3 font-semibold">
+        tabs_in_pills
+      </div>
       <TabsIP />
+      <div className="text-xl animate-bounce  m-3 font-semibold">
+        tabs_with_underline_and_badges
+      </div>
       <TabsWUB />
+      <div className="text-xl animate-bounce  m-3 font-semibold">
+        tabs_with_underline_and_icons
+      </div>
       <TabsWUAI />
+      <div className="text-xl animate-bounce  m-3 font-semibold">
+        tabs_with_underline
+      </div>
       <TabsWUL />
     </>
   )
