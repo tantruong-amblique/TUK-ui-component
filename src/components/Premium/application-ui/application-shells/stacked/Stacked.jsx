a4f8f7471daee7e7c57c6bc08efc618fa8f8e673
Akash52 <ac8572611@gmail.com>
2021-06-05 16:38:25 +0530

    Stacked5

--- src/components/Premium/application-ui/application-shells/stacked/Stacked.jsx
+++ src/components/Premium/application-ui/application-shells/stacked/Stacked.jsx
@@ -10,20 +10,47 @@ import StackNavBarEight from './light_nav_with_bottom_border'
 import StackNavBarNine from './two_row_navigation_with_overlap'
 
 const Stacked = () => {
-    return (
-        <div>
-            <StackNavBarOne />
-            <StackNavBarTwo />
-            <StackNavBarThree />
-            <StackNavBarFour />
-            <StackNavBarFive />
-            <StackNavBarSix />
-            <StackNavBarSeven />
-            <StackNavBarEight />
-            <StackNavBarNine />
-            
-        </div>
-    )
+  return (
+    <div>
+      <div className="text-4xl animate-pulse m-3 font-semibold">Stacked</div>
+      <div className="text-xl animate-bounce  m-3 font-semibold">
+        brand_nav_with_overlap
+      </div>
+      <StackNavBarOne />
+      <div className="text-xl animate-bounce  m-3 font-semibold">
+        branded_nav_with_compact_white_page_header
+      </div>
+      <StackNavBarTwo />
+      <div className="text-xl animate-bounce  m-3 font-semibold">
+        branded_nav_with_white_page_header
+      </div>
+      <StackNavBarThree />
+      <div className="text-xl animate-bounce  m-3 font-semibold">
+        dark_nav_with_compact_white_page_header
+      </div>
+      <StackNavBarFour />
+      <div className="text-xl animate-bounce  m-3 font-semibold">
+        dark_nav_with_overlap
+      </div>
+      <StackNavBarFive />
+      <div className="text-xl animate-bounce  m-3 font-semibold">
+        dark_nav_with_white_page_header
+      </div>
+      <StackNavBarSix />
+      <div className="text-xl animate-bounce  m-3 font-semibold">
+        light_nav_on_gray_background
+      </div>
+      <StackNavBarSeven />
+      <div className="text-xl animate-bounce  m-3 font-semibold">
+        light_nav_with_bottom_border
+      </div>
+      <StackNavBarEight />
+      <div className="text-xl animate-bounce  m-3 font-semibold">
+        two_row_navigation_with_overlap
+      </div>
+      <StackNavBarNine />
+    </div>
+  )
 }
 
 export default Stacked
